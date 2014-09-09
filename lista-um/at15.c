84e7db62d6dcd5bbda29cbe35428856c5e88996c
TiagoSevero <tiago.reissevero@gmail.com>
2014-09-09 17:00:26 -0300

    Lista de Exercícios

---
+++ lista-um/at15.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(){
+
+	int a,b,e,f;
+	int *c,*d;
+	c= &a;
+	d= &b;
+	
+	printf("\n%p\n", c);
+	printf("%p\n\n", d);	
+
+	if( c > d)
+		printf("A eh maior= %p\n\n",c);
+	else
+		printf("B eh maior = %p\n\n",d);
+
+	return 0;
+}
