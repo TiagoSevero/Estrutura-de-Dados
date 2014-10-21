4126653261fc8a99c8d6f8c025f9a2d2c1825d8f
TiagoSevero <tiago.reissevero@gmail.com>
2014-10-21 10:57:00 -0300

    9

---
+++ lista-um/at9.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int vet1[] = {2,3,4,5,6};
+int vet2[] = {2,3,4,5,6};
+
+
+int soma_vetor(int *vetor1, int *vetor2){
+	
+	static int vet3[5];
+	static int *v1, *v2,*v3, i;
+	int j;
+
+	int l = sizeof (vet1) /sizeof (int);
+	int l2 = sizeof (vet2) /sizeof (int);
+
+	if(l!=l2){
+		printf("Erro\n");
+		return 1;	
+	}	
+	else{
+
+		v1 = vet1;	
+		v2 = vet2;	
+		v3 = vet3;			
+		for(i=0;i<5;i++){
+
+			*v3 = *v1 + *v2;
+	
+			v1++;	
+			v2++;		
+			v3++;
+					
+		}
+		
+			/*UTILIZADO PARA TESTAR SE O VETOR ESTÁ SENDO PREENCHIDO
+			for(j=0;j<5;j++)
+				printf("%d ", vet3[j]);
+			*/
+		return 0;		
+	}	
+	  	
+}
+
+
+
+
+
+
+int main(){
+
+	
+	int result; 
+	
+	 result = soma_vetor(vet1, vet2);	
+
+	 if (result == 0)	
+		 printf("\n%d\n",result);
+	
+}
+	
+
