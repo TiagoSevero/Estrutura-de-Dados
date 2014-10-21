03b4a56e09a24d4d154ecc7ee6a17881d2543b42
TiagoSevero <tiago.reissevero@gmail.com>
2014-10-21 11:15:36 -0300

    7

---
+++ lista-dois/at7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+char *str1;
+
+char *strcopy(char *str){
+	
+		
+	str = "Hello World";
+	strcpy(str1,str);	
+	
+	return str1;
+} 
+
+
+
+int main(){
+
+	char *ch;
+	char *resultado;
+	
+	resultado = strcopy(ch);
+
+	printf("%s\n", resultado);	
+	return 0;
+}
