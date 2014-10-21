3c2648434a5ca7c8e68fbde27eb3aa2c073c3204
TiagoSevero <tiago.reissevero@gmail.com>
2014-10-21 11:08:48 -0300

    1

---
+++ lista-dois/at1.c
@@ -0,0 +1,7 @@
+/*
+
+	A diferença conceitual é que v quando expresso por v[3] representa o conteúdo de v na posição 3, isto é, se um vetor é declaro, por exemplo, "v[] = {3,4,5,6,7};" v[3] representa o valor contido na posição 4 (quatro) do vetor, no exemplo em questão v[3] = 6. Já v+3 representa o endereço de memória referente a posição 4 (quatro) do vetor e não o conteúdo do mesmo.
+	 
+
+
+*/
