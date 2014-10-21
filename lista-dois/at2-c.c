537b1f720b5f29e0c9c5e0c815da708afc2784b2
TiagoSevero <tiago.reissevero@gmail.com>
2014-10-21 11:12:26 -0300

    2c

---
+++ lista-dois/at2-c.c
@@ -0,0 +1,21 @@
+/*
+	O código abaixo não apresenta erros 
+
+*/
+
+
+
+#include<stdio.h>
+
+main(){
+
+	char *a, *b; 
+	a = "abacate";  
+	b = "uva";  
+
+	if (a < b) 
+		 printf ("%s vem antes de %s no dicionário", a, b); 
+	else 
+		 printf ("%s vem depois de %s no dicionário", a, b); 
+
+}
